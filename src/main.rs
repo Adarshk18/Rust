c33d49c88e12b2b071e74505b102b0952c47c7ec
Adarshk18 <adarshkohli183@gmail.com>
2025-07-29 13:00:02 +0530

    new

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+#[derive(Debug)]
+
+struct Deck {
+    cards: Vec<String>,
+}
+
+impl Deck{
+    fn new() -> Self{
+        //list of 'suits'-'hearts','spades'
+    let suits= vec!["Hearts","Spades","Diamonds"];
+    let values = vec!["Ace","Two","Three"];
+
+    let mut cards = vec![];
+
+    for suit in suits{
+        for value in &values{
+            let card = format!("{} of {}",value,suit);
+            cards.push(card);
+        }
+    }
+
+
+    //a variable is a bindings
+    let deck = Deck {cards};
+    return deck;
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    
+let deck = Deck::new();
+    println!("Here's your deck: {:#?}",deck);
 }
