d57fef4a1c24919e84f2ca1ccb79710beafc31bf
Adarshk18 <adarshkohli183@gmail.com>
2025-09-24 19:32:11 +0530

    new

--- src/main2.rs
+++ src/main2.rs
@@ -1,19 +1,29 @@
-fn five() -> i32 {
-    45
-}
+// fn five() -> i32 {
+//     45
+// }
 
 fn main(){
 
 
     //functions that returns something
-    let x = five();
-    println!("The value of x is: {x}");
+    // let x = five();
+    // println!("The value of x is: {x}");
     
-    another_function(3, 'c');
+    // another_function(3, 'c');
+
+    /*Branches */
+
+    let number = 5;
+
+    if number <5 {
+        println!("conditionwas true");
+    }else{
+        println!("condition was false");
+    }
 
 }
 
-fn another_function(x: i32, unit_label: char){
-    println!("The Value is: {x}{unit_label}");
+// fn another_function(x: i32, unit_label: char){
+//     println!("The Value is: {x}{unit_label}");
 
-}
+// }
