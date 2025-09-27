75eb40539d0c7dc02cd5fd35f442be539535867e
Adarshk18 <adarshkohli183@gmail.com>
2025-09-27 16:49:32 +0530

    new

--- src/ownership&borrowing/main.rs
+++ src/ownership&borrowing/main.rs
@@ -14,20 +14,36 @@ fn main() {
     // println!("s = {s}, s1 = {s1}");
 
     /*ownership function */
-    let s = String::from("hello");
+    // let s = String::from("hello");
 
-    takes_ownership(s);
+    // takes_ownership(s);
 
-    let x= 4;
-    makes_copy(x);
-    
+    // let x= 4;
+    // makes_copy(x);
+
+    let s1 = gives_ownership();
+
+    let s2 = String::from("hello");
+    let s3 = takes_and_gives_back(s2);
+
 
 }
 
-fn takes_ownership(some_string: String){
-    println!("{some_string}");
+// fn takes_ownership(some_string: String){
+//     println!("{some_string}");
+// }
+
+// fn makes_copy(some_integer: i32){
+//     println!("{some_integer}")
+// }
+
+fn gives_ownership() -> String{
+    let some_string = String::from("yours");
+    some_string
 }
 
-fn makes_copy(some_integer: i32){
-    println!("{some_integer}")
+
+fn takes_and_gives_back(a_string: String) -> String{
+    a_string
 }
+
