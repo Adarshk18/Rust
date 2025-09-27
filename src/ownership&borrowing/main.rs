81324a0080308ead1faaaa2710d3068725372c33
Adarshk18 <adarshkohli183@gmail.com>
2025-09-27 16:04:09 +0530

    new

--- src/ownership&borrowing/main.rs
+++ src/ownership&borrowing/main.rs
@@ -1,7 +1,15 @@
 fn main() {
-    let mut s = String::from("hello");
-    println!("{}", s);
+    // let mut s = String::from("hello");
+    // println!("{}", s);
 
-    s.push_str(", world!");
-    println!("{s}");
+    // s.push_str(", world!");
+    // println!("{s}");
+
+    // let mut s = String::from("hello");
+    // s= String::from("adarsh");
+    // println!("{s}");
+
+    let s = String::from("hello");
+    let s1 = s.clone();
+    println!("s = {s}, s1 = {s1}");
 }
