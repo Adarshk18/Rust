400006ec2af21b38ee507c9454b28627ed07c1cb
Adarshk18 <adarshkohli183@gmail.com>
2025-10-08 20:11:04 +0530

    new

--- src/structs/struct1.rs
+++ src/structs/struct1.rs
@@ -14,4 +14,25 @@ fn main(){
 		email: String::from("adarsh@gmail.com"),
 		sign_in_count: 1,
 	};
+
+	//if user1 is mutable we can change the value of its fields
+	let mut user2 = User{
+		active: true,
+		username: String::from("adarsh1"),
+		email:String::from("adarsh1@gmail.com"),
+		sign_in_count: 2,
+	};
+
+	user1.email = String::from("adarsh3@gmail.com");
 }
+
+fn build_user(username: String, email: String) -> User{
+	User{
+		active: true,
+		username,
+		email,
+		sign_in_count: 1,
+	}
+}
+
+
