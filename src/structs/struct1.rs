fe684f4f7db409e24f216bfe1b4af7cc7e5854d5
Adarshk18 <adarshkohli183@gmail.com>
2025-10-08 20:39:34 +0530

    new

--- src/structs/struct1.rs
+++ src/structs/struct1.rs
@@ -16,14 +16,27 @@ fn main(){
 	};
 
 	//if user1 is mutable we can change the value of its fields
-	let mut user2 = User{
-		active: true,
-		username: String::from("adarsh1"),
-		email:String::from("adarsh1@gmail.com"),
-		sign_in_count: 2,
-	};
+	// let mut user2 = User{
+	// 	active: true,
+	// 	username: String::from("adarsh1"),
+	// 	email:String::from("adarsh1@gmail.com"),
+	// 	sign_in_count: 2,
+	// };
 
-	user1.email = String::from("adarsh3@gmail.com");
+	// user1.email = String::from("adarsh3@gmail.com");
+
+	// let user2 = User{
+	// 	active: user1.active,
+	// 	username: user1.username,
+	// 	email: String::from("adarsg3@gmail.com"),
+	// 	sign_in_count: user1.sign_in_count,
+	// };
+
+	//another way fo doing the above
+	let user2 = User{
+		email: String::from("adarsh654@gmail.com"),
+		..user1
+	};
 }
 
 fn build_user(username: String, email: String) -> User{
