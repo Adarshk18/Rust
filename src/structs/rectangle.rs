6d657b8b0902b26c328d0ca65f4c0d16b3d2380e
Adarshk18 <adarshkohli183@gmail.com>
2025-10-12 17:34:38 +0530

    new

--- src/structs/rectangle.rs
+++ src/structs/rectangle.rs
@@ -13,12 +13,37 @@
 
 //using tuples
 
+// fn main(){
+//     let rect1 = (30,50);
+
+//     println!("The area of reactangle is {} square pixels.", area(rect1));
+// }
+
+// fn area(dimensions: (u32,u32)) -> u32{
+//     dimensions.0*dimensions.1
+// }
+
+/*some other way adding more meaning */
+
+#[derive(Debug)]
+struct Rectangle{
+    width: u32,
+    height: u32,
+}
+
 fn main(){
-    let rect1 = (30,50);
+    let scale = 2;
+    let rect1 = Rectangle{
+        width: debug!(30*scale),
+        height: 50,
+    };
 
-    println!("The area of reactangle is {} square pixels.", area(rect1));
+    // println!("The area of rectangle is {} square pixels.", area(&rect1));
+    // println!("rect1 is {rect1:?}");
+
+    dbg!(&rect1);
 }
 
-fn area(dimensions: (u32,u32)) -> u32{
-    dimensions.0*dimensions.1
-}
+// fn area(rectangle: &Rectangle) -> u32{
+//     rectangle.widht*rectangle.height
+// }
