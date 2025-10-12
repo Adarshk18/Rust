8935500a019566d865d5191f281f79ca33749a83
Adarshk18 <adarshkohli183@gmail.com>
2025-10-12 11:47:16 +0530

    new

---
+++ src/structs/rectangle.rs
@@ -0,0 +1,24 @@
+// fn main(){
+//     let width1 = 30;
+//     let height1 = 50;
+
+//     println!("The area of rectangle is {} square pixels.", area(width1,height1));
+
+
+// }
+
+// fn area(width: u32, height: u32) -> u32{
+//     width*height
+// }
+
+//using tuples
+
+fn main(){
+    let rect1 = (30,50);
+
+    println!("The area of reactangle is {} square pixels.", area(rect1));
+}
+
+fn area(dimensions: (u32,u32)) -> u32{
+    dimensions.0*dimensions.1
+}
