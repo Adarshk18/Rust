e9ad272b014569886a0ea96991005c27b3c21535
Adarshk18 <adarshkohli183@gmail.com>
2025-10-23 18:49:25 +0530

    new

---
+++ src/structs/impl.rs
@@ -0,0 +1,21 @@
+struct Rect{
+    width: u32,
+    height: u32,
+}
+
+impl Rect{
+    fn area(&self) -> u32{
+        return self.widht * self.height
+    }
+    fn perimeter(&self) -> u32{
+        return 2*(self.width + self.height);
+    }
+}
+
+fn main(){
+    let rect: React = React{
+        width: 30,
+        height: 50,
+    };
+    println!("The area of rectangle ios {}",rect.area());
+}
