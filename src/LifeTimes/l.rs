dcfd3dfda4b2fb6aca1e339a6f3a4a1332b069b9
Adarshk18 <adarshkohli183@gmail.com>
2025-11-04 21:37:32 +0530

    new

--- src/LifeTimes/l.rs
+++ src/LifeTimes/l.rs
@@ -1,16 +1,20 @@
-fn longest(a:String, b: String) -> String{
-    if a.len() > b.len(){
-        return a
-    }else{
-        return b
+fn longest<'a>(a: &'a str, b: &'a str) -> &'a str {   //right now its taking ownership 
+    if a.len() > b.len() {
+        return a;
+    } else {
+        return b;
     }
 }
 
+//without ownership
 
-fn main(){
+
+fn main() {
     let longest_str;
     let str1 = String::from("small");
-    let str2 = String::from("longer");
-    longest_str = longest(str1,str2);
-    println!("{}",longest_str);
+    {
+        let str2 = String::from("longer");
+        longest_str = longest(&str1, &str2);  //as we have used lifetimes so longest_str lifetime ends here we cant use after this .
+    }
+    // println!("{}", longest_str);
 }
