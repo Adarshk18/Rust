1c52adc344770b2dee58ebae25b3b54512fb8a08
Adarshk18 <adarshkohli183@gmail.com>
2025-12-14 17:19:26 +0530

    new

--- src/Enums/1.rs
+++ src/Enums/1.rs
@@ -1,35 +1,23 @@
-// enum Direction{
-//     North,
-//     East,
-//     South,
-//     West,
-// }
-
-// fn main(){
-//     let my_direction: Direction = Direction::North;
-
-//     move_direction(my_direction);
-// }
-
-// fn move_direction(direction: Direction){
-
-// }
-
-enum Shape{
+enum Shape {
     Circle(f64),
     Square(f64),
-    Rectangle(f64,f64),
+    Rectangle(f64, f64),
 }
 
-fn calculate_area(shape: Shape) -> f64{
-    return 0;
+fn calculate_area(shape: Shape) -> f64 {
+    match shape {
+        Shape::Circle(radius) => std::f64::consts::PI * radius * radius,
+        Shape::Square(side) => side * side,
+        Shape::Rectangle(length, width) => length * width,
+    }
 }
 
-fn main(){
+fn main() {
     let circle: Shape = Shape::Circle(5.0);
     let square: Shape = Shape::Square(4.0);
-    let rectangle: Shape = Shape::Rectangle(3.0,6.0);
+    let rectangle: Shape = Shape::Rectangle(3.0, 6.0);
 
-    calculate_area(circle);
-    calculate_area(square); 
+    println!("Circle area: {}", calculate_area(circle));
+    println!("Square area: {}", calculate_area(square));
+    println!("Rectangle area: {}", calculate_area(rectangle));
 }
